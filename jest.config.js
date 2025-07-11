ea08f46d4e034e3c7f14709a82284634a6271db8
Lawrence <appdeveloper247@gmail.com>
2025-07-11 08:12:58 +0100

    chore: add ci/cd workflows

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ export default {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
